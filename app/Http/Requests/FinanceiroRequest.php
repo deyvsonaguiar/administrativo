40a6c5ef54f03b2ba685cd5e0a0f15b5554a6f7b
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2021-03-10 23:33:40 -0400

    Validando campos com FinanceiroRequest

---
+++ app/Http/Requests/FinanceiroRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class FinanceiroRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        return [
+            'descricao' => 'required|string|max:255',
+            'date' => 'date',
+            'valor' => 'required|numeric',
+            'tipo' => 'required',
+            'empresa_id' => 'required'
+        ];
+    }
+
+    //sobrescrevendo os campos para limpar as máscaras
+    public function validationData()
+    {
+
+        $campo = $this->all();
+
+        $campo['valor'] = str_replace(['.', ',' ],['','.'], $campo['valor']);
+        $campo['data'] = data_br_para_iso($campo['data']);
+
+
+        $this->replace($campo);
+
+        return $campo;
+    }
+
+}
